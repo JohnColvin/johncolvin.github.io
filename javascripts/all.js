2c4cea08ef9558b4847513baf9d61af6ceaa1902
John Colvin <john.colvin@neo.com>
2013-08-22 23:36:13 -0500

    middleman build

---
+++ javascripts/all.js
@@ -0,0 +1,8 @@
+
+document.getElementById("image").onmouseover = function() {
+  this.src = "/images/blue_steel.jpg"
+}
+document.getElementById("image").onmouseout = function() {
+  this.src = "/images/me.jpeg"
+}
+;
