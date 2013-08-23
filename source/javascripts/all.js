691d5c92cfac30de26f24c33d3c0c2d726de66ea
John Colvin <john.colvin@neo.com>
2013-08-22 23:07:28 -0500

    The whole site

---
+++ source/javascripts/all.js
@@ -0,0 +1,8 @@
+//= require_tree .
+
+document.getElementById("image").onmouseover = function() {
+  this.src = "/images/blue_steel.jpg"
+}
+document.getElementById("image").onmouseout = function() {
+  this.src = "/images/me.jpeg"
+}
