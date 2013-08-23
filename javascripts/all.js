12e1b83191884956cd330e50a087a3bd9330c761
John Colvin <john.colvin@neo.com>
2013-08-23 09:28:41 -0500

    New build

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,8 +1,1 @@
-
-document.getElementById("image").onmouseover = function() {
-  this.src = "/images/blue_steel.jpg"
-}
-document.getElementById("image").onmouseout = function() {
-  this.src = "/images/me.jpeg"
-}
-;
+document.getElementById("image").onmouseover=function(){this.src="/images/blue_steel.jpg"},document.getElementById("image").onmouseout=function(){this.src="/images/me.jpeg"};
